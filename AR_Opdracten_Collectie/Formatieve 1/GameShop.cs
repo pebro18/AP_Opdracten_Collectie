2ba0c71c2d3d622f9aa25025ebbdfb5433918251
pebro18 <pebro06@gmail.com>
2021-04-14 22:20:27 +0200

    Verder gewerkt aan GameShop

--- AR_Opdracten_Collectie/Formatieve 1/GameShop.cs
+++ AR_Opdracten_Collectie/Formatieve 1/GameShop.cs
@@ -13,16 +13,13 @@ namespace AR_Opdracten_Collectie.Formatieve
             int releaseJaar1 = DateTime.Now.Year - 1; // 1 jaar geleden
             int releaseJaar2 = DateTime.Now.Year - 2; // 2 jaar geleden
 
-
             Game g1 = new("Just Cause 3", releaseJaar1, 49.98);
             Game g2 = new("Need for Speed: Rivals", releaseJaar2, 45.99);
             Game g3 = new("Need for Speed: Rivals", releaseJaar2, 45.99);
 
-
             Persoon p1 = new("Eric", 200);
             Persoon p2 = new("Hans", 55);
             Persoon p3 = new("Arno", 185);
-
 
             // Druk de volgende transacties af (pas de code aan)
             p1.Koop(g1);
@@ -32,37 +29,46 @@ namespace AR_Opdracten_Collectie.Formatieve
             p2.Koop(g1);
             p3.Koop(g3);
 
-
             // Druk personen p1 p2 p3 nu af naar de console
             Console.WriteLine("p1: " + p1.name + " heeft een budget van " + p1.budget + " en bezit de volgende games:\n");
-            foreach (var item in p1.GameVerzameling)
-            {
-                int yeardiffrence = DateTime.Now.Year - item.releaseyear;
-                double oldprice = item.prijs * Math.Pow(0.70 ,yeardiffrence);
-                Console.WriteLine(item.name + ", uitgegeven in " + item.releaseyear + "; nieuwprijs:" + item.prijs + " nu voor: " + oldprice);
-            }
-            Console.WriteLine("p2: "+ p2.name + " heeft een budget van " + p2.budget + " en bezit de volgende games:\n");
-            foreach (var item in p2.GameVerzameling)
-            {
-                int yeardiffrence = DateTime.Now.Year - item.releaseyear;
-                double oldprice = item.prijs * Math.Pow(0.70, yeardiffrence);
-                Console.WriteLine(item.name + ", uitgegeven in " + item.releaseyear + "; nieuwprijs:" + item.prijs + " nu voor: " + oldprice);
-            }
+            PrintOutPersoonLibary(p1);
+
+            Console.WriteLine("p2: " + p2.name + " heeft een budget van " + p2.budget + " en bezit de volgende games:\n");
+            PrintOutPersoonLibary(p2);
+
             Console.WriteLine("p3: " + p3.name + " heeft een budget van " + p3.budget + " en bezit de volgende games:\n");
-            foreach (var item in p3.GameVerzameling)
-            {
-                int yeardiffrence = DateTime.Now.Year - item.releaseyear;
-                double oldprice = item.prijs * Math.Pow(0.70, yeardiffrence);
-                Console.WriteLine(item.name + ", uitgegeven in " + item.releaseyear + "; nieuwprijs:" + item.prijs + " nu voor: " + oldprice);
-            }
+            PrintOutPersoonLibary(p3);
 
             // Druk de volgende transacties af (pas de code aan)
             p1.Verkoop(g1, p3);
             p2.Verkoop(g2, p3);
             p2.Verkoop(g1, p1);
 
-
             // Druk personen p1 p2 p3 nu af naar de console
+            Console.WriteLine("p1: " + p1.name + " heeft een budget van " + p1.budget + " en bezit de volgende games:\n");
+            PrintOutPersoonLibary(p1);
+
+            Console.WriteLine("p2: " + p2.name + " heeft een budget van " + p2.budget + " en bezit de volgende games:\n");
+            PrintOutPersoonLibary(p2);
+
+            Console.WriteLine("p3: " + p3.name + " heeft een budget van " + p3.budget + " en bezit de volgende games:\n");
+            PrintOutPersoonLibary(p3);
+        }
+
+        private static void PrintOutPersoonLibary(Persoon _p)
+        {
+            foreach (var game in _p.gameVerzameling)
+            {
+                double newprice = GetGameNewPrice(game);
+                Console.WriteLine(game.name + ", uitgegeven in " + game.releaseyear + "; nieuwprijs:" + game.prijs + " nu voor: " + newprice + "\n");
+            }
+        }
+
+        public static double GetGameNewPrice(Game _game)
+        {
+            int yeardiffrence = DateTime.Now.Year - _game.releaseyear;
+            double newprice = _game.prijs * Math.Pow(0.70, yeardiffrence);
+            return newprice;
         }
     }
 
@@ -72,23 +78,21 @@ namespace AR_Opdracten_Collectie.Formatieve
         public int releaseyear;
         public double prijs;
 
-        public Game(string nm,int dt,double pr)
+        public Game(string nm, int dt, double pr)
         {
             name = nm;
             releaseyear = dt;
             prijs = pr;
         }
-
     }
 
     class Persoon
     {
         public string name;
         public double budget;
-        public List<Game> GameVerzameling = new List<Game>();
+        public List<Game> gameVerzameling = new();
 
-
-        public Persoon(string nm,int bd)
+        public Persoon(string nm, int bd)
         {
             name = nm;
             budget = bd;
@@ -96,12 +100,12 @@ namespace AR_Opdracten_Collectie.Formatieve
 
         public void Koop(Game _game)
         {
-            bool InVerzameling = GameVerzameling.Exists(x => x.name == _game.name);
+            bool InVerzameling = gameVerzameling.Exists(x => x.name == _game.name);
             if (budget >= _game.prijs && !InVerzameling)
             {
                 Console.WriteLine(name + " koopt " + _game.name + " gelukt");
-                budget -= _game.prijs;
-                GameVerzameling.Add(_game);
+                budget -= TestGameShop.GetGameNewPrice(_game);
+                gameVerzameling.Add(_game);
             }
             else
             {
@@ -111,8 +115,23 @@ namespace AR_Opdracten_Collectie.Formatieve
 
         public void Verkoop(Game _game, Persoon _anderepersoon)
         {
+            bool InVerzamelingVanAnder = _anderepersoon.gameVerzameling.Exists(x => x.name == _game.name);
+            if (_anderepersoon.budget >= _game.prijs && !InVerzamelingVanAnder)
+            {
+                double _gameprijs = TestGameShop.GetGameNewPrice(_game);
+
+                Console.WriteLine(name + " koopt " + _game.name + " gelukt");
+
+                _anderepersoon.budget -= _gameprijs;
+                budget += _gameprijs;
+                gameVerzameling.Remove(_game);
+                _anderepersoon.gameVerzameling.Add(_game);
+            }
+            else
+            {
+                Console.WriteLine(name + " koopt " + _game.name + " niet gelukt");
+            }
 
         }
-
     }
 }
