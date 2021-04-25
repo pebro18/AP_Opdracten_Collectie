4b7bef8a598389fb93675d3847d8495c587e677a
pebro18 <pebro06@gmail.com>
2021-04-25 11:37:56 +0200

    added skeleton of summatieve 1

---
+++ AR_Opdracten_Collectie/Summatieve 1/Zwemmer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AR_Opdracten_Collectie.Summatieve_1
+{
+    class Zwemmer : Persoon, IDeelnemer
+    {
+        public float SwimTime { get; set; }
+
+    }
+}
