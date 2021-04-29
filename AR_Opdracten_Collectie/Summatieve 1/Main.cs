f53fcb6335156e09a7176ee87c64c495493c3c96
pebro18 <pebro06@gmail.com>
2021-04-29 15:29:42 +0200

    added Main for summatieve 1

---
+++ AR_Opdracten_Collectie/Summatieve 1/Main.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AR_Opdracten_Collectie.Summatieve_1
+{
+    class StartUpMain
+    {
+        //static void Main()
+        //{
+        //    // initiallise all classes in vars
+
+        //    //Zwemmers
+        //    Zwemmer Zwemmer1 = new("Joris",20);
+        //    Zwemmer Zwemmer2 = new("Claudia",23);
+        //    Zwemmer Zwemmer3 = new("Rebekka",19);
+        //    Zwemmer Zwemmer4 = new("Martine",21);
+        //    Zwemmer Zwemmer5 = new("Fenna",20);
+        //    Zwemmer Zwemmer6 = new("Maartje",22);
+
+        //    //Sheidsrechter
+        //    Sheidsrechter Sheids1 = new("Arjan",35);
+        //    Sheidsrechter Sheids2 = new("Grietje",40);
+
+        //    // Wedstrijden
+
+        //    Zwemmer[] Deelnemers1 = new Zwemmer[] { };
+        //    Zwemmer[] Deelnemers2 = new Zwemmer[] { };
+        //    Zwemmer[] Deelnemers3 = new Zwemmer[] { };
+        //    Wedstrijd Wedstrijd1 = new(Deelnemers1,Sheids1,TypeZwemSlag.VrijeSlag);
+        //    Wedstrijd Wedstrijd2 = new(Deelnemers2,Sheids2, TypeZwemSlag.SchoolSlag);
+        //    Wedstrijd Wedstrijd3 = new(Deelnemers3,Sheids2, TypeZwemSlag.VlinderSlag);
+
+        //    // Tournooien
+        //    Wedstrijd[] Tournooi1Wedstrijden = new Wedstrijd[] { Wedstrijd1, Wedstrijd2, Wedstrijd3 };
+        //    Tournooi tournooi1 = new(Tournooi1Wedstrijden);
+
+        //    // Start Wedstrijden
+        //    // krijg resultaat
+
+        //    foreach (var wedstrijd in tournooi1.Wedstrijden)
+        //    {
+        //        wedstrijd.StartWedstrijd();
+        //        wedstrijd.ToString();
+        //    }
+
+        //}
+    }
+
+}
