d8eaeddfe49ffbb0648bc7f2c9a6d16fee212f93
pebro18 <pebro06@gmail.com>
2021-05-10 10:11:53 +0200

    part 2 formatieve 2 moved script to own project instance

---
+++ Formatieve 2/Formatieve 2/Node.cs
@@ -0,0 +1,27 @@
+﻿namespace AR_Opdracten_Collectie.Formatieve_2
+{
+    class Node
+    {
+        public string name;
+        public Node NodeA { set; get; }
+        public Node NodeB { set; get; }
+
+        public Node(string name)
+        {
+            this.name = name;
+        }
+
+        public Node GotoConnectingNode(char GoTo)
+        {
+            return GoTo switch
+            {
+                'A' => NodeA,
+                'B' => NodeB,
+                _ => null,
+            };
+
+        }
+
+    }
+
+}
