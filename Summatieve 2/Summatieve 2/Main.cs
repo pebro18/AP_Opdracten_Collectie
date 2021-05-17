5edbb8612b1316d3bb4767727c2980a866421f8c
pebro18 <pebro06@gmail.com>
2021-05-17 14:25:14 +0200

    Finished Main

--- Summatieve 2/Summatieve 2/Main.cs
+++ Summatieve 2/Summatieve 2/Main.cs
@@ -12,9 +12,29 @@ namespace AR_Opdracten_Collectie.Summatieve_2
             FSM StateMachiene = new(Amount);
             foreach (var node in StateMachiene.NodeList)
             {
-                Console.WriteLine($"{node.Name}: geef aan in nummers waaraan de node gaat verbinden");
+                Console.WriteLine($"Node {node.Name}: geef aan in nummers waaraan de node gaat verbinden en gebruik kommas om andere nummer aan te geven");
+                string InputString = Console.ReadLine();
+                string[] Numbers = InputString.Split(',');
+
+                List<Node> LinkingToNodes = new();
+                foreach (var number in Numbers)
+                {
+                    Node FoundNode = StateMachiene.NodeList.Find(x => x.Name == number);
+                    LinkingToNodes.Add(FoundNode);
+                }
+
+                StateMachiene.LinkNodeToOtherNodes(node, LinkingToNodes.ToArray());
             }
 
+            Console.WriteLine("Voer in Nummers met kommas tussen");
+            string InputCommands = Console.ReadLine();
+            string[] Commands = InputCommands.Split(',');
+            StateMachiene.GoToNextNode(Commands);
+
+            foreach (var Node in StateMachiene.NodeHistory)
+            {
+                Console.WriteLine(Node.Name);
+            }
         }
 
     }
