715011464620089452a7ed2f017b55097d78013d
pebro18 <pebro06@gmail.com>
2021-05-20 13:35:47 +0200

    Generated Summatieve3 base files

---
+++ Summatieve3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Summatieve3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
