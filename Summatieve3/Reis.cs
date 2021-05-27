7c1a282aa37ba8247d1036e4338daa1048e45521
pebro18 <pebro06@gmail.com>
2021-05-27 23:32:09 +0200

    This is not finished at all aaaaaaaaah stuff thats not finished have NotImplementedException

--- Summatieve3/Reis.cs
+++ Summatieve3/Reis.cs
@@ -4,20 +4,81 @@ using System.Linq;
 
 namespace Summatieve3
 {
-    class Reis : IComparable<Stap>
+    public class Reis : IComparable
     {
-        List<Stap> Stappen;
-        Queue<Stap> ReisRij;
+        public List<Stap> ReisLijst;
 
-        public bool CompareTo(Stap obj1, Stap obj2)
+        public Reis(int _amountSteps,string _typeReis)
         {
-            return false;
+            ReisLijst = GenerateStappen(_amountSteps,_typeReis);
+        }
+        private List<Stap> GenerateStappen(int _amountSteps, string _typeReis)
+        {
+            throw new NotImplementedException();
         }
 
-        public void AddStap(Stap _stap)
+        // used example for Dijkstra
+        // https://www.programiz.com/dsa/dijkstra-algorithm
+        public void CalculateAllPathsFromStart(Stap _start)
         {
-            Stappen.Add(_stap);
+            throw new NotImplementedException();
+            List<Stap> VistitedStap = new();
+            foreach (var stap in ReisLijst)
+            {
+                stap.SetCostOfSelf(int.MaxValue);
+            }
+            _start.SetCostOfSelf(0);
+
+
+            foreach (var stap1 in ReisLijst)
+            {
+
+                
+                foreach (var stap2 in ReisLijst)
+                {
+
+                }
+
+            }
         }
+
+        public Stap GetStepOfMinDistance(Stap _current,List<Stap> _visited)
+        {
+            throw new NotImplementedException();
+            Stap Output;
+            foreach (var Connected in _current.ConnectedStappen)
+            {
+                if (!_visited.Contains(Connected.Value))
+                {
+                    
+                }
+            }
+
+            return Output;
+        }
+
+        public void LinkStapWithOthers(Stap _target, (int,Stap)[] _stappen)
+        {
+            foreach (var stap in _stappen)
+            {
+                _target.AddConnectedStap(stap);
+            }
+        }
+
+        public int CompareTo(object _reisObj)
+        {
+            throw new NotImplementedException();
+            Reis compareObj = (Reis)_reisObj;
+
+            // niet dit maar vergelijkt kosten en reistijd en shit
+
+        }
+
+        public int GetReisCount()
+        {
+            return ReisLijst.Count();
+        }
+
 
     }
 }
